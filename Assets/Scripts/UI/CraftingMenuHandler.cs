5f8236df1af0025325c4c2a47f68cd9c02e87459
= <mikipux7@gmail.com>
2019-10-16 19:53:50 -0500

    Fixed cursor locking problem

---
+++ Assets/Scripts/UI/CraftingMenuHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+public class CraftingMenuHandler : MonoBehaviour
+{
+    public GameObject craftingMenu;
+    public FirstPersonController fpc;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void menuCheck()
+    {
+        bool menuKey = Input.GetKeyUp(KeyCode.E);
+
+        if (menuKey)
+        {
+            switch (craftingMenu.activeInHierarchy)
+            {
+                case true:
+                    craftingMenu.SetActive(false);
+                    fpc.m_MouseLook.SetCursorLock(true);
+                    break;
+                default:
+                    craftingMenu.SetActive(true);
+                    fpc.m_MouseLook.SetCursorLock(false);
+                    break;
+                    
+            }
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        menuCheck();
+    }
+}
