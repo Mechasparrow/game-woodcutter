49f808d2db4b7f1a54c108f5a51b8c57375dc111
= <mikipux7@gmail.com>
2019-10-28 18:47:52 -0500

    Beginnings of Procedural Tree generation

---
+++ Assets/Scripts/Trees/ProceduralTree.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralTree : MonoBehaviour
+{
+    public GameObject trunk;
+
+    public GameObject leafPrefab;
+
+    public Transform leafPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaleTrunk(0.5f);
+        spawnLeaf(leafPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void scaleTrunk(float scale)
+    {
+        Vector3 trunkScale = trunk.transform.localScale;
+        trunkScale.y *= scale;
+
+        trunk.transform.localScale = trunkScale;
+    }
+
+    void spawnLeaf(Transform transform)
+    {
+        GameObject newLeaf = Instantiate(leafPrefab);
+        newLeaf.transform.SetParent(transform);
+
+        newLeaf.transform.localScale = new Vector3(4, 4, 4);
+        newLeaf.transform.position = transform.position;
+    }
+
+}
