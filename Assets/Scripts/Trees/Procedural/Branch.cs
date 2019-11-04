825f0f249a603c1ee81eb1a95476c55e8cf49725
= <mikipux7@gmail.com>
2019-11-04 14:27:53 -0600

    randomized branches

--- Assets/Scripts/Trees/Procedural/Branch.cs
+++ Assets/Scripts/Trees/Procedural/Branch.cs
@@ -4,27 +4,21 @@ using UnityEngine;
 
 public class Branch : MonoBehaviour
 {
-    public GameObject leafs;
-
+    
     public GameObject leafPrefab;
 
     public Transform leafPosition;
 
     private void Start()
     {
-        
-    }
-
-    private void Update()
-    {
-        
-    }
+        spawnLeaf(leafPosition);   
+    }    
     
     private void spawnLeaf(Transform transform)
     {
         GameObject newLeaf = Instantiate(leafPrefab);
 
-        newLeaf.transform.SetParent(leafs.transform);
+        newLeaf.transform.SetParent(transform);
         newLeaf.transform.position = transform.position;
     }
 }
