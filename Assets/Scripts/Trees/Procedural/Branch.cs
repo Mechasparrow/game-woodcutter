894d2e2283d632c162454cc2622b0b7c008efacb
= <mikipux7@gmail.com>
2019-11-04 14:36:43 -0600

    Procedural trees have randomized leaf sizes

--- Assets/Scripts/Trees/Procedural/Branch.cs
+++ Assets/Scripts/Trees/Procedural/Branch.cs
@@ -11,14 +11,18 @@ public class Branch : MonoBehaviour
 
     private void Start()
     {
-        spawnLeaf(leafPosition);   
-    }    
-    
-    private void spawnLeaf(Transform transform)
+        spawnLeaf(leafPosition, 1, 4);   
+    }
+
+    private void spawnLeaf(Transform transform, float minSize, float maxSize)
     {
+        float leafoSize = Random.Range(minSize, maxSize);
+
         GameObject newLeaf = Instantiate(leafPrefab);
 
         newLeaf.transform.SetParent(transform);
+        newLeaf.transform.localScale = Vector3.one * leafoSize;
+
         newLeaf.transform.position = transform.position;
     }
 }
